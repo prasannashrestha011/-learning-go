fb9df190ab0b238fd6cef0441318f5912b1b0dba
prasannashrestha011 <shresthaprashanna27@gmail.com>
2025-01-24 19:56:29 +0545

    exps

--- cmd/pkgs/jwtConfigs/jwt.go
+++ cmd/pkgs/jwtConfigs/jwt.go
@@ -12,7 +12,7 @@ func CreateToken(username string) (string, error) {
 	jwtSecret := os.Getenv("JWT_SECRET")
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
 		"sub": username,
-		"exp": time.Now().Add(time.Minute * 24 * 30),
+		"exp": time.Now().Add(time.Minute * 15).Unix(),
 	})
 	tokenString, err := token.SignedString([]byte(jwtSecret))
 	if err != nil {
@@ -20,6 +20,7 @@ func CreateToken(username string) (string, error) {
 	}
 	return tokenString, nil
 }
+
 func ValidateToken(tokenString string) (bool, error) {
 	jwtSecret := os.Getenv("JWT_SECRET")
 	if jwtSecret == "" {
@@ -28,16 +29,35 @@ func ValidateToken(tokenString string) (bool, error) {
 
 	// Parse the token with the secret key
 	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
-		// Validate the signing method
+
 		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
 			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
 		}
 		return []byte(jwtSecret), nil
 	})
 
-	// Check if token is valid
-	if err != nil || !token.Valid {
+	if err != nil {
 		return false, err
+	}
+
+	if !token.Valid {
+		return false, fmt.Errorf("invalid token")
+	}
+
+	claims, ok := token.Claims.(jwt.MapClaims)
+	if !ok {
+		return false, fmt.Errorf("invalid token claims")
+	}
+
+	exp, ok := claims["exp"].(float64)
+	if !ok {
+		return false, fmt.Errorf("expiration claim (exp) missing or invalid in token")
+	}
+
+	// Convert float64 to time.Time
+	expTime := time.Unix(int64(exp), 0)
+	if time.Now().After(expTime) {
+		return false, fmt.Errorf("token is expired")
 	}
 
 	return true, nil
